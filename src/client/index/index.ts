2fef9e4c225fa820fc1f2b3c420d67b2cbeea438
Carles Company Soler <ccompany@bbsoftbalear.com>
2016-12-09 13:01:53 +0100

    Fix routing.

--- src/client/index/index.ts
+++ src/client/index/index.ts
@@ -8,14 +8,15 @@ import "bootstrap";
 import 'bootstrap/dist/css/bootstrap.css';
 
 angular.module('test',['ui.router'])
-    .config(($stateProvider:ng.ui.IStateProvider,$urlRouterProvider:ng.ui.IUrlRouterProvider)=>{
+    .config(['$stateProvider','$urlRouterProvider',($stateProvider:ng.ui.IStateProvider,$urlRouterProvider:ng.ui.IUrlRouterProvider)=>{
         $stateProvider.state('test',{
             url:'/',
-            controller: ['NumberService',TestController],
+            controller: 'TestController',
             controllerAs: 'vm',
-            template: '/index/test.html'
+            templateUrl: 'index/test.html'
         });
         $urlRouterProvider.when('','/');
-    })
+    }])
     .factory('NumberService',[()=>new NumberService()])
+    .controller('TestController',['NumberService',TestController])
     .controller('IndexController',[IndexController]);
